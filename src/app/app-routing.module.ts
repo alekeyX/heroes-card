d55adc8af3b8c33dc16453c55586fcb86bacb1df
Alejandro Rojas <alejandrod.rojasg@gmail.com>
2020-03-07 00:19:14 -0400

    actualizacion de componentes about, heroe, heroes y home, creacion de servicio de heroe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+import { HeroeComponent } from "./components/heroe/heroe.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
